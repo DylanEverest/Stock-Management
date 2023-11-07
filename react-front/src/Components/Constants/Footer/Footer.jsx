d11d56d7a63057252e3f798c6ff5c29740b8f777
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-07 13:41:03 +0300

    footer

---
+++ react-front/src/Components/Constants/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+export default function Footer() 
+{
+    return(
+        <footer class="main-footer">
+            <div class="row">
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-xl-start justify-content-lg-start justify-content-md-start justify-content-center">
+                    <p class="text-gradient-02">Design By Saerox</p>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-xl-end justify-content-lg-end justify-content-md-end justify-content-center">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link" >Documentation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" >Changelog</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+    
+}
--- react-front/src/Components/Gate/Stockstate.jsx
+++ react-front/src/Components/Gate/Stockstate.jsx
@@ -1,12 +1,15 @@
+import Footer from "../Constants/Footer/Footer";
 import Header from "../Constants/Header/Header";
 
 export default function Stockstate() 
 {
  
     return(
-        <div className="div">
+        <div>
 
             <Header/>
+
+            <Footer/>
 
         </div>
     )
