88308cc0f99085aa5a12ecc57050a55c8e0faa4a
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-07 22:10:51 +0300

    runner moved

--- stock-management/src/main/java/com/enterprise/stockmanagement/runner/StockManagementApplication.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/runner/StockManagementApplication.java
@@ -1,4 +1,4 @@
-package com.enterprise.stockmanagement;
+package com.enterprise.stockmanagement.runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
