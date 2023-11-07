740c06531691f84a7c584cf5ab307e1e6ae23623
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-08 00:14:52 +0300

    article entities

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Articles/Entities/Articles.java
@@ -0,0 +1,48 @@
+package com.enterprise.stockmanagement.Articles.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Articles 
+{
+
+    @Id   
+    private String articlesID ;
+
+    @Column(length = 250 ,nullable = false)
+    private String nameArticles ;
+
+    @ManyToOne(optional = false ,fetch = FetchType.EAGER)
+    private MethodType methodType ;
+
+
+    
+    public String getArticlesID() {
+        return articlesID;
+    }
+
+    public void setArticlesID(String articlesID) {
+        this.articlesID = articlesID;
+    }
+
+    public String getNameArticles() {
+        return nameArticles;
+    }
+
+    public void setNameArticles(String nameArticles) {
+        this.nameArticles = nameArticles;
+    }
+
+    public MethodType getMethodType() {
+        return methodType;
+    }
+
+    public void setMethodType(MethodType methodType) {
+        this.methodType = methodType;
+    }
+
+}
---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Articles/Entities/MethodType.java
@@ -0,0 +1,35 @@
+package com.enterprise.stockmanagement.Articles.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class MethodType 
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 250 ,nullable = false)
+    private String methodType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMethodType() {
+        return methodType;
+    }
+
+    public void setMethodType(String methodType) {
+        this.methodType = methodType;
+    }
+}
