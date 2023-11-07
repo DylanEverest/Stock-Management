3585f96f53ea67e416db16f29f08770088ddcbb0
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-07 20:20:14 +0300

    articles & magasins data

---
+++ react-front/src/Services/DataForms/Articles/Articles.jsx
@@ -0,0 +1,6 @@
+export default function Articles() 
+{
+    const articles = ["article1", "article2", "article3", "article"] ;
+ 
+    return articles ;
+}
---
+++ react-front/src/Services/DataForms/Magasins/Magasins.jsx
@@ -0,0 +1,8 @@
+export default function Magasins() 
+{
+
+    const magasins = ["article1", "article2", "article3", "article"] ;
+ 
+    return magasins ;
+
+}
