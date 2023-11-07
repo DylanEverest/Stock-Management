3f70403d75b87a8d50810b8ec1f1703e5bb6dfee
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-07 14:01:44 +0300

    side bar

---
+++ react-front/src/Components/Constants/Siderbar/Sidebar.jsx
@@ -0,0 +1,15 @@
+export default function Sidebar()
+{
+
+    return (
+        <div class="default-sidebar">
+            <nav class="side-navbar box-scroll sidebar-scroll" tabindex="1" >
+                <ul class="list-unstyled">
+                    <li><a href="components-widgets.html"><i class="la la-spinner"></i><span>Stock</span></a></li>
+                    <li><a href="components-widgets.html"><i class="la la-spinner"></i><span>Sortie de stock</span></a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+    
+}
