3490b627ce68796fa4596f1b4994ae2ea3f0b5f7
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-08 11:40:59 +0300

    articles update

--- stock-management/src/main/java/com/enterprise/stockmanagement/Articles/Entities/Articles.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Articles/Entities/Articles.java
@@ -3,6 +3,8 @@ package com.enterprise.stockmanagement.Articles.Entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -13,6 +15,7 @@ public class Articles
 {
 
     @Id   
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String articlesID ;
 
     @Column(length=250,nullable =  false) 
