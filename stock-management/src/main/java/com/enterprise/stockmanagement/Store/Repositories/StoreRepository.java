6bcf144e2d6538cae21630ca1554f200043908c9
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-08 11:49:52 +0300

    repo store

--- stock-management/src/main/java/com/enterprise/stockmanagement/Store/Repositories/StoreRepository.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Store/Repositories/StoreRepository.java
@@ -1,10 +1,13 @@
 package com.enterprise.stockmanagement.Store.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.enterprise.stockmanagement.Store.Entities.Store;
 
 public interface StoreRepository extends JpaRepository<Store,Integer>
 {
-    
+    Optional<Store> findByStoreName(@Param("storeName") String storeName);    
 }
