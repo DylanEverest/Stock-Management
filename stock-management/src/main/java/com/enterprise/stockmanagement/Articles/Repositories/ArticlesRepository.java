fb3e36c1863fc46612604af7bdb775f36eb63329
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-08 11:08:21 +0300

    new query

--- stock-management/src/main/java/com/enterprise/stockmanagement/Articles/Repositories/ArticlesRepository.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Articles/Repositories/ArticlesRepository.java
@@ -1,9 +1,16 @@
 package com.enterprise.stockmanagement.Articles.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.enterprise.stockmanagement.Articles.Entities.Articles;
 
 public interface ArticlesRepository extends JpaRepository<Articles, Integer>{
     
+    @Query("SELECT u FROM  articles u WHERE u.name_articles LIKE :family")
+    Optional<Articles> findArticlesLikeName(@Param("family") String family);
+
 }
