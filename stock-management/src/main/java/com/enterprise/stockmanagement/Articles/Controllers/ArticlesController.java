269e0467682459e8233255e1d8b4606e85ff76e1
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-08 09:30:17 +0300

    Articles: controller , DTO

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Articles/Controllers/ArticlesController.java
@@ -0,0 +1,72 @@
+package com.enterprise.stockmanagement.Articles.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enterprise.stockmanagement.Articles.DTO.ArticlesDTO;
+import com.enterprise.stockmanagement.Articles.Entities.Articles;
+import com.enterprise.stockmanagement.Articles.Services.CRUDArticles;
+
+@RestController
+@RequestMapping("/StockApplication")
+public class ArticlesController 
+{
+
+    @Autowired
+    private CRUDArticles crudArticles ;
+
+    @Autowired
+    private ArticlesDTO articlesDTO ;
+
+
+    @GetMapping(path = "/articles")
+    public ArticlesDTO getArticless()
+    {
+        List<Articles> articles = crudArticles.getArticlesList() ;
+        articlesDTO.setArticles(articles);
+        return articlesDTO ;
+    }
+
+    @GetMapping(path = "/articles/{id}")
+    public ArticlesDTO getArticlessById(@PathVariable("id") final Integer id)
+    {
+        Articles articles = crudArticles.getArticlesByID(Integer.valueOf(id)) ;
+        articlesDTO.setArticles(articles) ;
+
+        return articlesDTO ;
+    }
+
+    @PostMapping(path = "/articles")
+    public ArticlesDTO saveArticles(@RequestBody ArticlesDTO articlesDTO)
+    {
+        crudArticles.postArticles(articlesDTO.getArticles()[0]);
+
+        return articlesDTO;
+
+    }    
+
+    @PutMapping(path ="/articles/{id}")
+    public ArticlesDTO updateArticles(@PathVariable("id") final Integer id , @RequestBody ArticlesDTO articlesDTO)
+    {
+        crudArticles.updateArticles(id, articlesDTO.getArticles()[0]);
+
+        return articlesDTO;
+    }
+
+    @DeleteMapping(path = "/articles/{id}")
+    public void deleteArticles(@PathVariable("id") final Integer id)
+    {
+        crudArticles.deleteArticlesByID(id);
+    }
+    
+    
+}
--- stock-management/src/main/java/com/enterprise/stockmanagement/Articles/DTO/ArticlesDTO.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Articles/DTO/ArticlesDTO.java
@@ -1,5 +1,8 @@
 package com.enterprise.stockmanagement.Articles.DTO;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.enterprise.stockmanagement.Articles.Entities.Articles;
@@ -7,7 +10,7 @@ import com.enterprise.stockmanagement.Articles.Entities.Articles;
 @Component
 public class ArticlesDTO 
 {
-    
+    @Autowired
     Articles [] articles ;    
 
     String errors ;
@@ -16,8 +19,14 @@ public class ArticlesDTO
         return articles;
     }
 
+    public void setArticles(Articles articles) {
+        this.articles[0] = articles;
+    }
     public void setArticles(Articles[] articles) {
         this.articles = articles;
+    }
+    public void setArticles(List<Articles> articles) {
+        this.articles = articles.toArray(new Articles[articles.size()]);
     }
 
     public String getErrors() {
