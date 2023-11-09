51bfb9252189eb17a1d3eba0917e4d00f5696433
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-09 14:20:53 +0300

    StockStatusService

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/StockStatus/Services/StockStatusService.java
@@ -0,0 +1,15 @@
+package com.enterprise.stockmanagement.StockStatus.Services;
+
+import com.enterprise.stockmanagement.StockStatus.Models.FormStockStatusModel;
+import com.enterprise.stockmanagement.StockStatus.Models.StockStatusModel;
+
+public class StockStatusService 
+{
+
+    public StockStatusModel getStockStatus(FormStockStatusModel formStockStatusModel) {
+        
+        return null ;
+    }
+
+
+}
