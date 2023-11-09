4f075a3c631399556a20bc14e15982ec8406c6fa
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-09 17:39:02 +0300

    isValid

--- stock-management/src/main/java/com/enterprise/stockmanagement/DateValidation/Service/Implements/DateValidationService.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/DateValidation/Service/Implements/DateValidationService.java
@@ -1,9 +1,11 @@
 package com.enterprise.stockmanagement.DateValidation.Service.Implements;
 
+import com.enterprise.stockmanagement.DateValidation.Service.Implements.Exception.DateUnvalidExcetion;
+
 public interface DateValidationService <T>
 {
 
-    public T isValid(String date);
+    public T isValid(String date) throws DateUnvalidExcetion;
 
     public boolean isSuccessive(T date1 , T date2) ;
 
