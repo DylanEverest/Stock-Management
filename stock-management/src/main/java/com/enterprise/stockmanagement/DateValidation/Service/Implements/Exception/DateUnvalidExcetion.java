1bbb7a26157ddd437d21ffd0491a9364fd83aecd
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-09 17:38:46 +0300

    DateException

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/DateValidation/Service/Implements/Exception/DateUnvalidExcetion.java
@@ -0,0 +1,9 @@
+package com.enterprise.stockmanagement.DateValidation.Service.Implements.Exception;
+
+public class DateUnvalidExcetion extends Exception {
+
+    public DateUnvalidExcetion(String arg0) {
+        super(arg0);
+    }
+    
+}
---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/DateValidation/Service/Timestamp/TimestampUnvalidExcetion.java
@@ -0,0 +1,11 @@
+package com.enterprise.stockmanagement.DateValidation.Service.Timestamp;
+
+import com.enterprise.stockmanagement.DateValidation.Service.Implements.Exception.DateUnvalidExcetion;
+
+public class TimestampUnvalidExcetion extends DateUnvalidExcetion{
+
+    public TimestampUnvalidExcetion(String arg0) {
+        super(arg0);
+    }
+
+}
