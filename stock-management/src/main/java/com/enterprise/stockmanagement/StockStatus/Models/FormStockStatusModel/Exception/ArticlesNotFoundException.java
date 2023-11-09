e00e44d7d525c0fd531d0240ee22aafc4c4a89e3
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-09 20:24:11 +0300

    exception

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/StockStatus/Models/FormStockStatusModel/Exception/ArticlesNotFoundException.java
@@ -0,0 +1,9 @@
+package com.enterprise.stockmanagement.StockStatus.Models.FormStockStatusModel.Exception;
+
+public class ArticlesNotFoundException extends Exception{
+
+    public ArticlesNotFoundException(String arg0) {
+        super(arg0);
+    }
+
+}
---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/StockStatus/Models/FormStockStatusModel/Exception/StoreNotFoundException.java
@@ -0,0 +1,9 @@
+package com.enterprise.stockmanagement.StockStatus.Models.FormStockStatusModel.Exception;
+
+public class StoreNotFoundException extends Exception {
+
+    public StoreNotFoundException(String arg0) {
+        super(arg0);
+    }
+    
+}
