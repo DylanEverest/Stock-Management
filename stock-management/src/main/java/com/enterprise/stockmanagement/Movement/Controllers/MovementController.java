e4f571b388a8646665294890a2fcf1ffd613eebf
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-11 12:07:17 +0300

    controller

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Controllers/MovementController.java
@@ -0,0 +1,67 @@
+package com.enterprise.stockmanagement.Movement.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.enterprise.stockmanagement.Movement.DTO.FormMovementDTO;
+import com.enterprise.stockmanagement.Movement.Entities.Movement;
+import com.enterprise.stockmanagement.Movement.Service.in.MovementINService;
+import com.enterprise.stockmanagement.Movement.Service.out.MovementOUTService;
+
+@RestController
+
+public class MovementController {
+    
+    @Autowired MovementINService movementINService;
+
+    @Autowired MovementOUTService movementOUTService ;
+
+    @Autowired Movement movementModel ;
+
+
+    @PostMapping
+    public FormMovementDTO inStock(FormMovementDTO formMovementDTO){
+
+        try 
+        {
+            movementModel.setArticles(formMovementDTO.getArticle());
+            movementModel.setDateMovement(formMovementDTO.getMovementDate());
+            movementModel.setStore(formMovementDTO.getStore());
+            movementModel.setMethodType(1);
+
+            movementINService.in(movementModel) ;
+
+
+        } 
+        catch (Exception e) 
+        {
+            formMovementDTO.setErrors(e.getMessage());
+        }
+
+        return formMovementDTO;
+    }
+
+    @PostMapping
+    public FormMovementDTO outStock(FormMovementDTO formMovementDTO){
+
+        try 
+        {
+            movementModel.setArticles(formMovementDTO.getArticle());
+            movementModel.setDateMovement(formMovementDTO.getMovementDate());
+            movementModel.setStore(formMovementDTO.getStore());
+            movementModel.setMethodType(1);
+
+            movementOUTService.out(movementModel) ;
+            
+
+        } 
+        catch (Exception e) 
+        {
+            formMovementDTO.setErrors(e.getMessage());
+        }
+
+        return formMovementDTO;
+    }
+
+}
