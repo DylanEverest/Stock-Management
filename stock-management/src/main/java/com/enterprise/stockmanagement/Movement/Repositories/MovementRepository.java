b86fa61383b0a81379b335310dc26f0f791d5ae0
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-12 20:07:56 +0300

    query 	modified:   stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Repositories/MovementRepository.java

--- stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Repositories/MovementRepository.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Repositories/MovementRepository.java
@@ -1,8 +1,10 @@
 package com.enterprise.stockmanagement.Movement.Repositories;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +14,8 @@ import com.enterprise.stockmanagement.Movement.Entities.Movement;
 public interface MovementRepository extends JpaRepository< Movement,Integer>{
     
     List<Movement> findAllByMethodType (@Param("methodType") Integer methodType) ;
+
+
+    @Query(value =  "SELECT * FROM MOVEMENT WHERE date_movement < :date",nativeQuery=true)
+    List<Movement> findAllWithDateMovementInferiorTo(@Param("date") Timestamp date) ;
 }
