0e530762d562c48d79fcf90ae350be97f6405746
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-12 20:00:12 +0300

    Denormalization

--- stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Entities/Movement.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Entities/Movement.java
@@ -40,6 +40,8 @@ public class Movement
 
     private Double unitPrice;
 
+    private Double actualPriceStock ;
+
 // controll
 
     public void setDateMovement(String timestamp) throws TimestampUnvalidExcetion
@@ -115,6 +117,21 @@ public class Movement
 
     public void setUnitPrice(String unitPrice) {
         this.unitPrice = Double.valueOf(unitPrice) ;
+    }
+
+
+    public void setUnitPrice(Double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+
+    public Double getActualPriceStock() {
+        return actualPriceStock;
+    }
+
+
+    public void setActualPriceStock(Double actualPriceStock) {
+        this.actualPriceStock = actualPriceStock;
     }
 
 
