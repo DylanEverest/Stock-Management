b941304a8eacd38f3f13d45deab99ceb2857d9fc
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-13 11:02:53 +0300

    movementview jpql

--- stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Repositories/view/MovementViewRepository.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Repositories/view/MovementViewRepository.java
@@ -17,21 +17,21 @@ public interface MovementViewRepository extends JpaRepository< MovementView,Inte
     List<MovementView> findAllByMethodType (@Param("methodType") Integer methodType) ;
 
 
-    @Query(value =  "SELECT * FROM MOVEMENT WHERE date_movement <= :date and method_type != 3",nativeQuery=true)
+    @Query(value =  "SELECT * FROM movementview WHERE date_movement <= :date and method_type != 3",nativeQuery=true)
     List<MovementView> findAllButReportingWithDateMovementInferiorOrEqualTo(@Param("date") Timestamp date) ;
 
-    @Query(value = "SELECT max(date_movement) FROM MOVEMENT WHERE date_movement < :date and method_type = 3 "
+    @Query(value = "SELECT max(date_movement) FROM movementview WHERE date_movement < :date and method_type = 3 "
     , nativeQuery = true)
     Optional<Timestamp> findLastReportDate(@Param("date") Timestamp date);
 
 
-    @Query( value =  "SELECT * FROM MOVEMENT WHERE :date1 <= date_movement AND date_movement< :date2",
+    @Query( value =  "SELECT * FROM movementview WHERE :date1 <= date_movement AND date_movement< :date2",
             nativeQuery=true)
     List<MovementView> findAllWithDateMovementBetween(  @Param("date1") Timestamp date, 
                                                     @Param("date2") Timestamp date2 ) ;
 
 
-    @Query( value =  "SELECT * FROM MOVEMENT WHERE :date1 < date_movement AND date_movement< :date2",
+    @Query( value =  "SELECT * FROM movementview WHERE :date1 < date_movement AND date_movement< :date2",
             nativeQuery=true)
     List<MovementView> findAllWithDateMovementBetweenWithClosedInterval(  @Param("date1") Timestamp date, 
                                                     @Param("date2") Timestamp date2 ) ;
