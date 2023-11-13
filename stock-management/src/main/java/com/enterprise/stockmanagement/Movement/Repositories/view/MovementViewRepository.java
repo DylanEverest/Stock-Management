4f367f7ef4332e2c3cf421a7cee7f9853a0c86c1
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-13 11:11:56 +0300

    refacto jpql

--- stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Repositories/view/MovementViewRepository.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Repositories/view/MovementViewRepository.java
@@ -17,23 +17,27 @@ public interface MovementViewRepository extends JpaRepository< MovementView,Inte
     List<MovementView> findAllByMethodType (@Param("methodType") Integer methodType) ;
 
 
-    @Query(value =  "SELECT * FROM movementview WHERE date_movement <= :date and method_type != 3",nativeQuery=true)
-    List<MovementView> findAllButReportingWithDateMovementInferiorOrEqualTo(@Param("date") Timestamp date) ;
+    @Query(value =  "SELECT * FROM movementview WHERE date_movement <= :date and method_type != 3 and family like :family% and store_name = :store",nativeQuery=true)
+    List<MovementView> findAllButReportingWithDateMovementInferiorOrEqualTo(@Param("date") Timestamp date , @Param("family") String family ,@Param("store") String store) ;
 
-    @Query(value = "SELECT max(date_movement) FROM movementview WHERE date_movement < :date and method_type = 3 "
+    @Query(value = "SELECT max(date_movement) FROM movementview WHERE date_movement < :date and method_type = 3 and family like :family% and store_name = :store"
     , nativeQuery = true)
-    Optional<Timestamp> findLastReportDate(@Param("date") Timestamp date);
+    Optional<Timestamp> findLastReportDate(     @Param("date") Timestamp date , 
+                                                @Param("family") String family ,
+                                                @Param("store") String store);
 
 
-    @Query( value =  "SELECT * FROM movementview WHERE :date1 <= date_movement AND date_movement< :date2",
+    @Query( value =  "SELECT * FROM movementview WHERE :date1 <= date_movement AND date_movement< :date2 and family like :family% and store_name = :store",
             nativeQuery=true)
     List<MovementView> findAllWithDateMovementBetween(  @Param("date1") Timestamp date, 
-                                                    @Param("date2") Timestamp date2 ) ;
+                                                        @Param("date2") Timestamp date2 , 
+                                                        @Param("family") String family ,@Param("store") String store ) ;
 
 
-    @Query( value =  "SELECT * FROM movementview WHERE :date1 < date_movement AND date_movement< :date2",
+    @Query( value =  "SELECT * FROM movementview WHERE :date1 < date_movement AND date_movement< :date2 and family like :family% and store_name = :store",
             nativeQuery=true)
     List<MovementView> findAllWithDateMovementBetweenWithClosedInterval(  @Param("date1") Timestamp date, 
-                                                    @Param("date2") Timestamp date2 ) ;
+                                                    @Param("date2") Timestamp date2 , 
+                                                    @Param("family") String family ,@Param("store") String store ) ;
                                                     
 }
