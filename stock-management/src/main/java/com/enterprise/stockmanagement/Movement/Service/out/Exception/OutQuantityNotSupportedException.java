84fb197e12ea0cf11b2fc0cc2cb54175a1f3c475
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-13 14:44:11 +0300

    getPossibleOUT

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Service/out/Exception/OutQuantityNotSupportedException.java
@@ -0,0 +1,9 @@
+package com.enterprise.stockmanagement.Movement.Service.out.Exception;
+
+public class OutQuantityNotSupportedException extends Exception {
+
+    public OutQuantityNotSupportedException(String arg0) {
+        super(arg0);
+    }
+    
+}
--- stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Service/out/MovementOUTService.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/Movement/Service/out/MovementOUTService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.enterprise.stockmanagement.Movement.Entities.Movement;
+import com.enterprise.stockmanagement.Movement.Models.Remnant;
 import com.enterprise.stockmanagement.Movement.Repositories.MovementRepository;
+import com.enterprise.stockmanagement.Movement.Service.out.Exception.OutQuantityNotSupportedException;
 
 @Service
 public class MovementOUTService {
@@ -39,6 +41,42 @@ public class MovementOUTService {
         }
 
         movementRepository.saveAll(list) ;
+
+    }
+
+
+    
+    public Movement [] getPossibleOUT(Movement movementExcepted) throws OutQuantityNotSupportedException 
+    {
+        List<Movement> possibleOut = new ArrayList<Movement>() ;
+        Double quantityTarget = movementExcepted.getQuantity();
+        List <Movement> entryList , outList ;
+        entryList =movementRepository.findAllByMethodType(1) ;
+        outList = movementRepository.findAllByMethodType(2) ;
+
+        Remnant[] remnants = new Remnant().getRemnants(entryList.toArray(new Movement[entryList.size()]),
+                                                        new Movement[outList.size()]
+                                                      ) ;
+        for (int i = 0; i < remnants.length; i++) {
+            Movement movement = new Movement();
+            movement.setArticles(movementExcepted.getArticles());
+            movement.setDateMovement(movementExcepted.getDateMovement());
+            movement.setStore(movementExcepted.getStore());
+
+            if (remnants[i].getQuantity()>=quantityTarget) 
+            {
+                movement.setUnitPrice(remnants[i].getUnitPrice());
+                movement.setQuantity(remnants[i].getQuantity());
+                possibleOut.add(movement);                
+
+                return possibleOut.toArray(new Movement[possibleOut.size()]);
+            }
+            else
+            {
+                quantityTarget = quantityTarget -remnants[i].getQuantity() ;
+            }
+        }
+        throw new OutQuantityNotSupportedException("The stock quantity is missing "+quantityTarget);
 
     }
 }
