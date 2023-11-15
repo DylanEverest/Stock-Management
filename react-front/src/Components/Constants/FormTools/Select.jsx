b8eea0af0708dd99b84c60408eaa8111aa63c5c8
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-15 09:42:13 +0300

    front

--- react-front/src/Components/Constants/FormTools/Select.jsx
+++ react-front/src/Components/Constants/FormTools/Select.jsx
@@ -4,7 +4,7 @@ export default function SelectForm(props) {
         <div class="form-group row mb-5">
             <label class="col-lg-3 form-control-label">{props.label}</label>
             <div class="col-lg-9 select mb-3">
-                <select name="account" class="custom-select form-control">
+                <select name={props.label} class="custom-select form-control">
                     {props.option.map( (opt, index)=>(
                         <option key={index}> {opt} </option>    
                     ))}
