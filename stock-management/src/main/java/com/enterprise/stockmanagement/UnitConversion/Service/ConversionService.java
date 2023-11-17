057807a856cf54eea37d94c5e0c9f66e8558b30e
Ratianarivo Ny Voara Hir' Ainatsara <ratianarivodylan@gmail.com>
2023-11-17 05:31:52 +0300

    conversion

---
+++ stock-management/src/main/java/com/enterprise/stockmanagement/UnitConversion/Service/ConversionService.java
@@ -0,0 +1,53 @@
+package com.enterprise.stockmanagement.UnitConversion.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.enterprise.stockmanagement.Movement.Entities.Movement;
+import com.enterprise.stockmanagement.UnitConversion.entity.Unit;
+import com.enterprise.stockmanagement.UnitConversion.repository.ConversionRepository;
+import com.enterprise.stockmanagement.UnitConversion.repository.UnitRepository;
+
+@Service
+public class ConversionService {
+
+    @Autowired
+    ConversionRepository conversionRepository;
+
+    @Autowired UnitRepository unitRepository ;
+
+
+    public void conversion (Movement model ,String unitDefault) throws Exception
+    {
+        Double factor = getFactor(model.getArticles().getUnit(), unitDefault) ;
+
+        if (factor == null) {
+            throw new Exception("no formula for the unit") ;
+        }
+        model.setQuantity( model.getQuantity() /factor);
+    }
+    
+    public Double getFactor(Unit unitTarget  , String unitDefault) throws Exception
+    {
+        // alaina ilay unite anle unitDefault
+       Unit defaultUnit ;
+        try 
+        {
+          defaultUnit = unitRepository.findByName(unitDefault).get() ;
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e.getMessage()) ;
+
+            throw new Exception("Not valid unit for "+unitDefault) ;
+        }
+        
+        // alaina ilay facteur de conversion
+        Double factor = conversionRepository.findFactor(unitTarget.getUnitId(),defaultUnit.getUnitId()) ;
+
+
+
+        return factor ;
+    }
+    
+}
--- stock-management/src/main/java/com/enterprise/stockmanagement/UnitConversion/repository/ConversionRepository.java
+++ stock-management/src/main/java/com/enterprise/stockmanagement/UnitConversion/repository/ConversionRepository.java
@@ -1,9 +1,17 @@
 package com.enterprise.stockmanagement.UnitConversion.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.enterprise.stockmanagement.UnitConversion.entity.Conversion;
 
 public interface ConversionRepository extends JpaRepository<Conversion,Integer>{
+
+
+    @Query(value = "select factor from conversion where unit_destination_unit_id = :dest and unit_source_unit_id = :source "
+           , nativeQuery = true)
+    Double findFactor(@Param("dest") Integer unit_destination_unit_id ,@Param("source") Integer source_unit_id) ;
     
 }
